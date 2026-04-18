cf52af072f42fc4732f2d9ad987a585a0b947d80
vythao181226-netizen <vythao181226@gmail.com>
2026-04-18 16:59:32 +0700

    Update BTN.py

--- BTN.py
+++ BTN.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPUgy1a/f/YMQumpMag5x97",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
